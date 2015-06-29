40d99375ab77ada1269e99702c6c88689956d3e5
KeylorDuran <kodurana@est.utn.ac.cr>
2015-06-29 09:28:14 -0600

    - Archivos
    Cambios en los archivos generales del proyecto.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,10 +23,6 @@ class ComposerAutoloaderInit131158d2c19f3e1bbde72cf077da0855
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit131158d2c19f3e1bbde72cf077da0855', 'loadClassLoader'));
 
-        $includePaths = require __DIR__ . '/include_paths.php';
-        array_push($includePaths, get_include_path());
-        set_include_path(join(PATH_SEPARATOR, $includePaths));
-
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
