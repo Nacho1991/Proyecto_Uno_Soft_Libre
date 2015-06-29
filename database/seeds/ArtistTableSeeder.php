09b77030ff7fa5016af256c327801c01c76d0776
Nacho1991 <ivalerio2222@hotmail.com>
2015-06-29 07:12:04 -0600

    Seeder Artist
    - Se generaron cambios en la clase Seeder para la tabla Artist

--- database/seeds/ArtistTableSeeder.php
+++ database/seeds/ArtistTableSeeder.php
@@ -1,20 +1,21 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
+use Faker\Factory as FakerFactory;
 
-class ArtistTableSeeder extends Seeder{
-
-    public function run(){
-
-        $faker = Faker::create();
-        for($i=0;$i<30;$i++) {
-            $id=\DB::table('artist')->insert(array(
+class ArtistTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $faker = FakerFactory::create();
+        for ($i = 0; $i < 30; $i++) {
+            $id = \DB::table('artist')->insert(array(
                 'name' => $faker->firstName
             ));
-            \DB::table('track')->insert(array(
-               'name'=>$faker->firstName,
-                'id_artist'=>$id
+            \DB::table('song')->insert(array(
+                'name' => $faker->firstName,
+                'url_source' => '/home/ignacio/ProyectoCyberLab/public/558e675e3a517.mp3',
+                'artist_id' => $id
             ));
         }
     }
