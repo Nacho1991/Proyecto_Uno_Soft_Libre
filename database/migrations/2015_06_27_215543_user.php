6ca7297f46092fc41e146c8d7214e32d95050bf7
Nacho1991 <ivalerio2222@hotmail.com>
2015-06-29 07:10:27 -0600

    User migration
    - Creación de la clase migración para la tabla User

---
+++ database/migrations/2015_06_27_215543_user.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class User extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user',function(Blueprint $table){
+            $valor = false;
+            $table->increments('id');
+            $table->string('email')->nullable();
+            $table->string('password')->nullable();
+            $table->boolean('is_admin')->nullable()->default($valor);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user');
+    }
+}
