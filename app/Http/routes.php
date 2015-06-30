3d490e26955ec3cae371315c266cca991bb59d47
KeylorDuran <kodurana@est.utn.ac.cr>
2015-06-30 01:24:38 -0600

    Rutas
    - Se establecieron las rutas necesarias para el acceso de los diferentes
    controladores del proyecto.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,37 @@
 |
 */
 
+/*Rutas para Login*/
 Route::get('/', function () {
-    return view('welcome');
+    return view('user.login');
 });
+
+/*Rutas para Home*/
+Route::get('inicio', function () {
+    return view('home.home');
+});
+
+/*Rutas para UserController*/
+Route::resource('users', 'UserController');
+Route::post('register', 'UserController@store');
+Route::post('logon', 'UserController@logon');
+Route::get('logout', 'UserController@logout');
+
+/*Rutas para cantantes*/
+Route::resource('artists', 'ArtistController');
+Route::post('artists/{id}/edit', 'ArtistController@edit');
+
+/*Rutas para canciones*/
+Route::resource('songs', 'SongController');
+Route::get('songs/{id}/delete', 'SongController@destroy');
+Route::post('songs/{id}/update', 'SongController@update');
+
+/*Rutas para lista de canciones personalizadas*/
+Route::resource('listas','ListaController');
+Route::get('listas/{id}/select','ListaController@select');
+Route::get('listas/{id}/destroy','ListaController@destroy');
+Route::get('listas/{id}/store','ListaController@store');
+
+/*Rutas para playlist*/
+Route::resource('playlists', 'PlayListController');
+Route::get('playlists/{id}/select', 'PlayListController@select');
