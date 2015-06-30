527e002ea1e595493b5547029db8bf4a4b1e5601
KeylorDuran <kodurana@est.utn.ac.cr>
2015-06-30 01:29:13 -0600

    Controlador User
    - Se creo el controlador User para el inicio y cierre de sesión.

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Input;
+use Hash;
+use Illuminate\Support\Facades\Redirect;
+use Auth;
+use Illuminate\Session;
+use App\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        return \View::make('user.register');
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        $email = Input::get('email');
+        $password = Input::get('password');
+        $password = Hash::make($password);
+        $user = new User();
+        $user->email = $email;
+        $user->password = $password;
+        $user->save();
+        return Redirect::to('/');
+
+    }
+
+    public function logon()
+    {
+
+        $userdata = array(
+            'email' => Input::get('email'),
+            'password' => Input::get('password')
+        );
+        if (Auth::attempt($userdata)) {
+            if (Auth::user()->is_admin) {
+                return Redirect::to('inicio');
+            }
+            /*Si es guest lo manda a la lista de canciones*/
+            return Redirect::to('inicio');
+        } else {
+            // validation not successful, send back to form
+            return Redirect::to('/')->withErrors(array('message' => 'Acceso Denegado'));
+        }
+
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return Redirect::to(url());
+    }
+}
